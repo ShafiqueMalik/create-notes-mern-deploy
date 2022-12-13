4314fd9c20b41e1845cdae680e8e9fd2d874170b
ShafiqueMalik <56837884+ShafiqueMalik@users.noreply.github.com>
2022-12-14 02:16:55 +0500

    updating port in  index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(errorHandler);
 
 const CONNECTION_URL = process.env.MONGODB_URL
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`))
 mongoose.set('strictQuery', true);
 //stop warnings from console
