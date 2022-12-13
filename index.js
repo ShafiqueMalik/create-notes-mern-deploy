b6c06cef7f1569ee02febed203bcd0b53ce75c70
ShafiqueMalik <56837884+ShafiqueMalik@users.noreply.github.com>
2022-12-14 02:10:31 +0500

    strickquery mongoose added

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const CONNECTION_URL = process.env.MONGODB_URL
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`))
-
+mongoose.set('strictQuery', true);
 //stop warnings from console
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT} and mongodb connected`)))
